1516e1efc0ce00a7e9831e3e4487a5ef890f169c
Alexander Ivankov <ffmoneyff@gmail.com>
2021-07-02 18:55:37 +0300

    ChangesTest

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,9 +1,10 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -12,23 +13,27 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+    int days;
 
-    LocalDate today = LocalDate.now();
-    LocalDate newDate = today.plusDays(3);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public String dayOfMeeting(int days) {
+        LocalDateTime today = LocalDateTime.now().plusDays(days);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String minDate = dateFormat.format(today);
+        return minDate;
+    }
 
     @Test
     void shouldSumbitRequest() {
         open("http://localhost:9999/");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Киров");
-        $("[data-test-id=date] input").doubleClick().sendKeys(formatter.format(newDate));
+        $("[data-test-id=date] [type='tel']").doubleClick().setValue(dayOfMeeting(3));
         form.$("[data-test-id=name] input").setValue("Виктор Иванов");
         form.$("[data-test-id=phone] input").setValue("+79896464784");
         form.$("[data-test-id=agreement").click();
         $$("button").find(exactText("Забронировать")).click();
-        Configuration.timeout = 15000;
-        $(withText("Успешно")).shouldBe(visible);
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + dayOfMeeting(3)));
     }
 
 }
